b4fb0f6f2ae3721efe34575e587481c9154912ef
gitjona <89576059+gitjona@users.noreply.github.com>
2021-10-07 13:00:31 +0300

    Delete booklist_secure_tests directory

--- booklist_secure_tests/src/main/java/fi/haagahelia/course/domain/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package fi.haagahelia.course.domain;
-
-import java.util.List;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryRepository extends CrudRepository<Category, Long>
-{
-	List<Category> findByName(String name);
-}
